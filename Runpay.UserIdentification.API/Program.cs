952d8d0be12c58a59a0bfa94204b96b15c9cd9fb
vvorobjev <v.vorobjev@hq.runpay.com>
2024-10-04 15:36:53 +0300

    add logging

--- Runpay.UserIdentification.API/Program.cs
+++ Runpay.UserIdentification.API/Program.cs
@@ -1,34 +1,30 @@
+using NLog.Extensions.Logging;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLogOptions"));
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    loggingBuilder.AddNLog();
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/endpoint", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    app.Logger.LogError("Check error log from worker.");
+    var forecast = "Project started;";
     return forecast;
 });
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
